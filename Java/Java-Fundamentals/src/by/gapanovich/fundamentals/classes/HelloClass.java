b657266f039b98a082ac1f6d9988875e40ca3ee1
Eugene Gapanovich <evgengapanovich@gmail.com>
2020-11-14 09:59:09 +0300

    fundamentals: main task

---
+++ Java/Java-Fundamentals/src/by/gapanovich/fundamentals/classes/HelloClass.java
@@ -0,0 +1,7 @@
+package by.gapanovich.fundamentals.classes;
+
+public class HelloClass {
+    public void helloUser(String userName){
+        System.out.println("Hello " + userName + "!" + "\nHave a nice day!");
+    }
+}

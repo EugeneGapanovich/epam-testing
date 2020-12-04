167f2c9317c1481927c3ee2b1908a113c1c399b8
Eugene Gapanovich <evgengapanovich@gmail.com>
2020-12-05 00:08:30 +0300

    add: lab WebDriver

---
+++ WebDriver/src/test/java/AddingItemToCartTest.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddingItemToCartTest {
+    private WebDriver webDriver;
+    private static final String URL = "https://sila.by/bt/multivarki/REDMOND/rmc-m90";
+
+    @BeforeTest (alwaysRun = true)
+    public void browserSetup(){
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        webDriver.get(URL);
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void browserTearDown(){
+        webDriver.quit();
+        webDriver = null;
+    }
+
+    @Test
+    public void addingItemToCartTest(){
+        String expectedItemName = "Мультиварка REDMOND RMC-M90 (код товара 38583)";
+        WebElement buttonAddItemToCart = webDriver.findElement(By.xpath("//div[@class='btn_zak']"));
+        buttonAddItemToCart.click();
+        WebElement item = (new WebDriverWait(webDriver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='tb_popup_korz']/div/div/span")));
+        String actualItemName = item.getText();
+        Assert.assertEquals(expectedItemName, actualItemName);
+    }
+}

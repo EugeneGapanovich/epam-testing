792dfc1afe1a8ed927cda052f3d69385a4958f1c
Eugene Gapanovich <evgengapanovich@gmail.com>
2020-12-27 16:27:54 +0300

    fixed logic in SortingTest()

--- Framework/src/test/java/by/gapanovich/framework/test/SortingTest.java
+++ Framework/src/test/java/by/gapanovich/framework/test/SortingTest.java
@@ -16,7 +16,7 @@ import static org.hamcrest.Matchers.*;
 public class SortingTest extends CommonConditions {
 
     private static final String TVS_URL = "https://sila.by/av/televizory";
-    private static final String MUTICOOKER_URL = "https://sila.by/bt/multivarki";
+    private static final String MULTICOOKER_URL = "https://sila.by/bt/multivarki";
     private static final String UPPER_PRICE_LIMIT = "250.00";
 
     @Test
@@ -81,7 +81,7 @@ public class SortingTest extends CommonConditions {
 
     @Test
     public void sortProductsByUpperPriceLimit(){
-        List<WebElement> products = new ListProductPage(driver, MUTICOOKER_URL)
+        List<WebElement> products = new ListProductPage(driver, MULTICOOKER_URL)
                 .openPage()
                 .insertValueInInputUpperPriceLimitField(UPPER_PRICE_LIMIT)
                 .buttonShowListClick()
@@ -89,19 +89,7 @@ public class SortingTest extends CommonConditions {
                 .buttonSortStartWithExpensiveClick()
                 .getProductsPrice();
 
-        List<WebElement> copyProducts = products;
-        copyProducts.sort(Comparator.comparing(product -> DoubleUtil.parseStringToDouble(product.getText())));
-        Collections.reverse(copyProducts);
-
-        boolean areTwoElementsEqual = true;
-        for (int i = 0; i < products.size(); i++) {
-            if (DoubleUtil.parseStringToDouble(products.get(i).getText()) !=
-                    DoubleUtil.parseStringToDouble(copyProducts.get(i).getText())) {
-                areTwoElementsEqual = false;
-                break;
-            }
-        }
-
-        Assert.assertTrue(areTwoElementsEqual);
+        Assert.assertTrue(DoubleUtil.parseStringToDouble(UPPER_PRICE_LIMIT) >=
+                DoubleUtil.parseStringToDouble(products.get(0).getText()));
     }
 }
